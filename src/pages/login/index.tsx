cfbe503eb79916b21b08dc020314ba1c265cd16c
AlexCeng <izengjing.cn@gmail.com>
2021-06-22 18:03:14 +0800

    add login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styles from './index.less';
+import classNames from 'classnames';
 
 class Login extends React.Component {
   constructor(props: any) {
@@ -6,6 +8,54 @@ class Login extends React.Component {
   }
 
   render() {
-    return <>Login</>;
+    return React.createElement(
+      'div',
+      {
+        className: styles.login,
+      },
+      React.createElement(
+        'div',
+        {
+          className: styles['login-wrapper'],
+        },
+        React.createElement(
+          'h2',
+          {
+            className: styles.title,
+          },
+          'Login',
+        ),
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'username',
+          className: classNames(
+            styles.input,
+            styles.name,
+            styles['neumorphism-inset'],
+          ),
+        }),
+        React.createElement('input', {
+          type: 'password',
+          placeholder: 'password',
+          className: classNames(
+            styles.input,
+            styles.password,
+            styles['neumorphism-inset'],
+          ),
+        }),
+        React.createElement('input', {
+          type: 'button',
+          value: 'Login',
+          className: classNames(
+            styles.input,
+            styles.button,
+            styles['neumorphism'],
+            styles.submit,
+          ),
+        }),
+      ),
+    );
   }
 }
+
+export default Login;
