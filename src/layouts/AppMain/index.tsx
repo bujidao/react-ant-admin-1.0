53f25a49e70e878427b62176d9802ef7302ade3b
Alex Ceng <izengjing.cn@gmail.com>
2021-06-26 12:21:49 +0800

    add WaterMark

--- src/layouts/AppMain/index.tsx
+++ src/layouts/AppMain/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import styles from './index.less';
+import WaterMark from 'watermark-component-for-react';
+import defaultSettings from '../../../config/defaultSettings';
 
 class AppMain extends React.Component {
   render() {
-    return <div className={styles['app-main']}>{this.props.children}</div>;
+    return React.createElement(
+      WaterMark,
+      {
+        content: defaultSettings.title,
+        style: {
+          height: '100%',
+        },
+        globalAlpha: 0.005,
+      },
+      React.createElement(
+        'div',
+        {
+          className: styles.appMain,
+        },
+        this.props.children,
+      ),
+    );
   }
 }
 
